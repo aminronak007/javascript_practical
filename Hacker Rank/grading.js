f6194a98d016680dd5ee26fabe5b3dd790d87e76
Uss Dev 1 <ussdev1@unitesoftwaresolutions.com>
2024-04-15 12:52:19 +0530

    hacker rank puzzles

---
+++ Hacker Rank/grading.js
@@ -0,0 +1,20 @@
+function gradingStudents(grades) {
+  let roundedGrades = [];
+  for (let grade of grades) {
+    if (grade < 38) {
+      roundedGrades.push(grade);
+    } else {
+      let nextMultipleOf5 = Math.ceil(grade / 5) * 5;
+      if (nextMultipleOf5 - grade < 3) {
+        roundedGrades.push(nextMultipleOf5);
+      } else {
+        roundedGrades.push(grade);
+      }
+    }
+  }
+  return roundedGrades;
+}
+
+let grades = [73, 67, 38, 33];
+let result = gradingStudents(grades);
+console.log("result", result);
